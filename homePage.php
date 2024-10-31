76199a499a2bbf34dac10b453d1bc49487fcd5f9
madison <mgibbons989@gmail.com>
2024-10-30 22:41:23 -0400

    updated home and login page and mundane.css

---
+++ homePage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeopardy</title>
+    <link rel = "stylesheet" href = "mundane.css">
+</head>
+<body>
+    <div class = "top">
+        HOME
+    </div>
+
+    <div class = "instructions">
+
+
+    </div>
+    
+    <div class = "center">
+        <div class = "welcome">
+            <p>Welcome to Web Jeopardy <!--<php $_POST['username'] -->!</p>
+            <p>Please select a topic below:</p>
+        </div>
+
+        <div class = "cards">
+            <div class = "topic">
+                <a href = ""><span class = "words">Disney</span></a>
+            </div>
+
+            <div class = "topic">
+                <a href = ""><span class = "words">Topic 2</span></a>
+            </div>
+
+            <div class = "topic">
+                <a href = ""><span class = "words">Topic 3</span></a>
+            </div>
+        </div>
+
+        <div class = "bot">
+            <a href = "loginPage.html" class = "logout">Logout</span></a>
+        </div>
+
+    </div>
+
+</body>
+</html>
